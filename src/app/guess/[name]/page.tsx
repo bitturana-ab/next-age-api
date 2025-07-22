0d381ef1a12cb2c82f583a0fce23e39fb4f7752a
bitturana-ab <b2ra830@gmail.com>
2025-07-22 16:00:39 +0530

    fetch error

--- src/app/guess/[name]/page.tsx
+++ src/app/guess/[name]/page.tsx
@@ -1,52 +1,56 @@
 // "use server"
 
-const getGender  = async (name:string) =>{
-    const res = await fetch(`https://api.genderize.io/?name=${name}`)
-    return res.json();
-}
+const getGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
 
-const getAge  = async (name:string) =>{
-    const res = await fetch(`https://api.agify.io/?name=${name}`)
-    return res.json();
-}
+const getAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+  return res.json();
+};
 
-const getNation  = async (name:string) =>{
-    const res = await fetch(`https://api.nationalize.io/?name=${name}`)
-    return res.json();
-}
+const getNation = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+  return res.json();
+};
 
 interface Params {
-    params: {name : string}
+  params: { name: string };
 }
 
-export default async function page ({params}:Params){
-    const name = (await params).name;
-    const ageData = getAge(name);
-    const genderData = getGender(name);
-    const nationData = getNation(name);
-    // console.log((await ageData).toString())
+export default async function page({ params }: Params) {
+  const name = (await params).name;
+  const ageData = await getAge(name);
+  const genderData = await getGender(name);
+  const nationData = await getNation(name);
+  // console.log((await ageData).toString())
 
-    const [age,gender,nation] = await Promise.all([ageData,genderData,nationData]);
+  //   const [age, gender, nation] = await Promise.all([
+  //     ageData,
+  //     genderData,
+  //     nationData,
+  //   ]);
 
-    // console.log(age);
-    // console.log(gender)
-    // console.log(nation)
-    return(
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+  // console.log(age);
+  // console.log(gender)
+  // console.log(nation)
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
       <div className="p-8">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
           Personal Info
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Age: {age?.age}
+          Age: {ageData?.age}
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Gender: {gender?.gender}
+          Gender: {genderData?.gender}
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Nationality: {nation?.country?.country_id}
+          Nationality: {nationData?.country?.country_id}
         </div>
       </div>
     </div>
-    )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
-"use client"
+"use client";
 
-import { useState ,FormEvent} from "react";
+import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [inputName, setInputName] = useState("");
+  const { push } = useRouter();
 
-  const [inputName,setInputName] = useState("");
-  const {push} = useRouter();
-
-  const submitBtnHandler = (e:FormEvent)=>{
+  const submitBtnHandler = (e: FormEvent) => {
     e.preventDefault();
-    if(inputName)
-    push(`/guess/${inputName}`)
-    
-  }
+    if (inputName) push(`/guess/${inputName}`);
+  };
 
   return (
     <div className=" bg-gray-800 p-30  items-center justify-center grid">
@@ -21,18 +18,20 @@ export default function Home() {
         <h1>Enter your name: </h1>
       </div>
       <form onSubmit={submitBtnHandler}>
-        <input 
-        type="text" 
-        className="bg-gray-400 px-2.5 mr-2 py-1 rounded-2xl"
-        placeholder="Enter name."
-        value={inputName}
-        onChange={(e)=>setInputName(e.target.value)}
+        <input
+          type="text"
+          className="bg-gray-400 px-2.5 mr-2 py-1 rounded-2xl"
+          placeholder="Enter name."
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}
         />
-        <button 
-        className="bg-blue-400 px-2.5 py-1 text-black rounded-2xl"
-        type="submit"
-        // onClick={submitBtnHandler}
-        >Guess age of {inputName}</button>
+        <button
+          className="bg-blue-400 px-2.5 py-1 text-black rounded-2xl"
+          type="submit"
+          // onClick={submitBtnHandler}
+        >
+          Guess age of {inputName}
+        </button>
       </form>
     </div>
   );
