e85406d6cd553b8521f44c974ab9836dee04444e
bitturana-ab <b2ra830@gmail.com>
2025-07-22 10:30:39 +0530

    added api and fetch

--- src/app/guess/[name]/page.tsx
+++ src/app/guess/[name]/page.tsx
@@ -1,10 +1,49 @@
+// "use server"
 
-export default function page ({params}:any){
+const getGender  = async (name:string) =>{
+    const res = await fetch(`https://api.genderize.io/?name=${name}`)
+    return res.json();
+}
+
+const getAge  = async (name:string) =>{
+    const res = await fetch(`https://api.agify.io/?name=${name}`)
+    return res.json();
+}
+
+const getNation  = async (name:string) =>{
+    const res = await fetch(`https://api.nationalize.io/?name=${name}`)
+    return res.json();
+}
+
+interface Params {
+    params: {name : string}
+}
+
+export default async function page ({params}:Params){
+    const name = (await params).name;
+    const ageData = getAge( name);
+    const genderData = getGender(name);
+    const nationData = getNation(name);
+    // console.log((await ageData).toString())
+
+    const [age,gender,nation] = await Promise.all([ageData,genderData,nationData]);
+
+    console.log(age);
+    console.log(gender)
+    console.log(nation)
     return(
         <div>
             <div>
-                <h1>Hello {params.name}</h1>
+                <h1>Hello {name}</h1>
+                <div>Personal Info</div>
+                <div>Total people of this name : {age?.count}</div>
+            <div>
+                Your predicted age: { age?.age}
             </div>
+            <div>Your pedictes gender: {gender?.gender}</div>
+            {/* <div>Your predicted nation: {nation?.country[0]?.country_id}</div> */}
+            </div>
+            
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,20 +16,20 @@ export default function Home() {
   }
 
   return (
-    <div className="p-30">
-      <div>
+    <div className=" bg-gray-800 p-30  items-center justify-center grid">
+      <div className="py-3 px-2 ">
         <h1>Enter your name: </h1>
       </div>
       <form onSubmit={submitBtnHandler}>
         <input 
         type="text" 
-        className="bg-gray-400 px-2.5 py-1 rounded-2xl"
+        className="bg-gray-400 px-2.5 mr-2 py-1 rounded-2xl"
         placeholder="Enter name."
         value={inputName}
         onChange={(e)=>setInputName(e.target.value)}
         />
         <button 
-        className="bg-blue-400 px-2.5 py-1 rounded-2xl"
+        className="bg-blue-400 px-2.5 py-1 text-black rounded-2xl"
         type="submit"
         // onClick={submitBtnHandler}
         >Guess age of {inputName}</button>
